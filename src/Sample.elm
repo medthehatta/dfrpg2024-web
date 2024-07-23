53980359b28739baf379291328b342ef8a0add1d
Med Mahmoud <medthehatta@gmail.com>
2024-07-23 07:40:06 -0400

    Add module for keeping sample data

---
+++ src/Sample.elm
@@ -0,0 +1,231 @@
+module Sample exposing (..)
+
+gameResult = """
+{
+    "ok": true,
+    "result": {
+        "entities": {
+            "Ice_Troll": {
+                "name": "Ice_Troll",
+                "fate": 2,
+                "refresh": 0,
+                "aspects": [
+                    {"kind": "mild", "name": "light tongue lashing", "tags": 0}
+                ],
+                "stress": {
+                    "physical": {
+                        "checked": [4, 5],
+                        "max": 8
+                    },
+                    "mental": {
+                        "checked": [],
+                        "max": 4
+                    },
+                    "hunger": {
+                        "checked": [
+                            3
+                        ],
+                        "max": 4
+                    }
+                }
+            },
+            "Cero": {
+                "name": "Cero",
+                "fate": 1,
+                "refresh": 0,
+                "aspects": [],
+                "stress": {
+                    "physical": {
+                        "checked": [],
+                        "max": 4
+                    },
+                    "mental": {
+                        "checked": [],
+                        "max": 3
+                    },
+                    "hunger": {
+                        "checked": [],
+                        "max": 3
+                    }
+                }
+            },
+            "Jackson": {
+                "name": "Jackson",
+                "fate": 1,
+                "refresh": 0,
+                "aspects": [],
+                "stress": {
+                    "physical": {
+                        "checked": [],
+                        "max": 6
+                    },
+                    "mental": {
+                        "checked": [],
+                        "max": 4
+                    },
+                    "hunger": {
+                        "checked": [],
+                        "max": 4
+                    }
+                }
+            },
+            "Jiang": {
+                "name": "Jiang",
+                "fate": 6,
+                "refresh": 0,
+                "aspects": [],
+                "stress": {
+                    "physical": {
+                        "checked": [],
+                        "max": 4
+                    },
+                    "mental": {
+                        "checked": [],
+                        "max": 4
+                    },
+                    "hunger": {
+                        "checked": [],
+                        "max": 4
+                    }
+                }
+            },
+            "Mal": {
+                "name": "Mal",
+                "fate": 8,
+                "refresh": 0,
+                "aspects": [],
+                "stress": {
+                    "physical": {
+                        "checked": [],
+                        "max": 4
+                    },
+                    "mental": {
+                        "checked": [],
+                        "max": 4
+                    },
+                    "hunger": {
+                        "checked": [],
+                        "max": 3
+                    }
+                }
+            },
+            "Nick": {
+                "name": "Nick",
+                "fate": 1,
+                "refresh": 0,
+                "aspects": [],
+                "stress": {
+                    "physical": {
+                        "checked": [],
+                        "max": 3
+                    },
+                    "mental": {
+                        "checked": [],
+                        "max": 4
+                    },
+                    "hunger": {
+                        "checked": [
+                            2
+                        ],
+                        "max": 4
+                    }
+                }
+            },
+            "Sparky": {
+                "name": "Sparky",
+                "fate": 0,
+                "refresh": 0,
+                "aspects": [],
+                "stress": {
+                    "physical": {
+                        "checked": [],
+                        "max": 8
+                    },
+                    "mental": {
+                        "checked": [],
+                        "max": 4
+                    },
+                    "hunger": {
+                        "checked": [],
+                        "max": 3
+                    }
+                }
+            },
+            "Weft": {
+                "name": "Weft",
+                "fate": 2,
+                "refresh": 0,
+                "aspects": [],
+                "stress": {
+                    "physical": {
+                        "checked": [],
+                        "max": 4
+                    },
+                    "mental": {
+                        "checked": [],
+                        "max": 4
+                    },
+                    "hunger": {
+                        "checked": [],
+                        "max": 4
+                    }
+                }
+            },
+            "scene": {
+                "name": "scene",
+                "fate": 0,
+                "refresh": 0,
+                "aspects": [],
+                "stress": {}
+            },
+            "Maw": {
+                "name": "Maw",
+                "fate": 0,
+                "refresh": 0,
+                "aspects": [],
+                "stress": {
+                    "physical": {
+                        "checked": [],
+                        "max": 10
+                    }
+                }
+            },
+            "plant1": {
+                "name": "plant1",
+                "fate": 0,
+                "refresh": 0,
+                "aspects": [],
+                "stress": {}
+            },
+            "plant2": {
+                "name": "plant2",
+                "fate": 0,
+                "refresh": 0,
+                "aspects": [],
+                "stress": {}
+            },
+            "plant4": {
+                "name": "plant4",
+                "fate": 0,
+                "refresh": 0,
+                "aspects": [],
+                "stress": {}
+            },
+            "plant5": {
+                "name": "plant5",
+                "fate": 0,
+                "refresh": 0,
+                "aspects": [],
+                "stress": {}
+            }
+        },
+        "order": {
+            "entities": [],
+            "bonuses": {},
+            "order": [],
+            "current": null,
+            "deferred": []
+        }
+    }
+}
+"""
